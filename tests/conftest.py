c15dfef4188b431f1857aa527f0116dcb6816ad2
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-04-20 16:24:49 +0300

    Добавление ветки для запуска в Jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,37 @@
 import pytest
 
-from selene import browser
+from selene import browser, Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
 @pytest.fixture(scope='function')
-def browser_option():
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+@pytest.fixture(scope='function')
+def browser_option(setup_browser):
     browser.config.base_url = 'https://github.com'
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
-
-    yield
-
-    browser.quit()
