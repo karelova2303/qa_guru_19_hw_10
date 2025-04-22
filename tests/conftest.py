415a175511dd426a3afb70f1932066eeee8f74dc
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-04-22 21:42:37 +0300

    обновлен conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,11 +15,6 @@ def load_env():
     load_dotenv()
 
 
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
-
-
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
@@ -33,6 +28,11 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
