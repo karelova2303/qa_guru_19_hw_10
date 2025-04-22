6b2d30c25dcbe7159bc103418985daafb429ee28
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-04-22 21:34:44 +0300

    обновлен conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,23 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 
 from selene import browser, Browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from utils import attach
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 
 @pytest.fixture(scope='function')
@@ -21,7 +34,7 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
 
@@ -34,3 +47,5 @@ def setup_browser(request):
     attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
+
+    browser.quit()
