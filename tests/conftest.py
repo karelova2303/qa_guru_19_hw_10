a7d3014bff3513866dfdb929512093642caf42b6
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-04-23 20:16:35 +0300

    обновлен conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+from email.policy import default
 
 import pytest
 from dotenv import load_dotenv
@@ -9,32 +10,39 @@ from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
+DEFAULT_BROWSER_VERSION = '128.0'
 
-@pytest.fixture(scope="session", autouse=True)
+def pytest_addoption(parser):
+    parser.addoption('--browser_version', default='128.0')
+
+
+@pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "128.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True,
-            "screenResolution": "1280x1024x24"
+            'browserName': 'chrome',
+            'browserVersion': browser_version,
+            'selenoid:options': {
+                'enableVNC': True,
+                'enableVideo': True,
+                'screenResolution': '1280x1024x24'
         }
     }
     options.capabilities.update(selenoid_capabilities)
 
-    selenoid_login = os.getenv("SELENOID_LOGIN")
-    selenoid_pass = os.getenv("SELENOID_PASS")
-    selenoid_url = os.getenv("SELENOID_URL")
+    selenoid_login = os.getenv('SELENOID_LOGIN')
+    selenoid_pass = os.getenv('SELENOID_PASS')
+    selenoid_url = os.getenv('SELENOID_URL')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
         options=options
     )
 
